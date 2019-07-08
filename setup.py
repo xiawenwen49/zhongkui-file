78fe87188aa90b93aa2935446740157b978d0294
jyker@dell <jyk.kongkong@gmail.com>
2019-07-08 19:55:34 +0800

    add package

---
+++ setup.py
@@ -0,0 +1,60 @@
+import codecs
+import os
+import re
+import sys
+from distutils.util import strtobool
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    """
+    Provide a Test runner to be used from setup.py to run unit tests
+    """
+
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import shlex
+        import pytest
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+def open_local(paths, mode="r", encoding="utf8"):
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), *paths)
+
+    return codecs.open(path, mode, encoding)
+
+
+with open_local(["sanic", "__init__.py"], encoding="latin1") as fp:
+    try:
+        version = re.findall(r"^__version__ = \"([^']+)\"\r?$", fp.read(),
+                             re.M)[0]
+    except IndexError:
+        raise RuntimeError("Unable to determine version.")
+
+setup(
+    name="zhongkui-file",
+    version=version,
+    url="https://git.kongkongss.com/jyker/zhongkui-file",
+    author="Kongkong Jiang",
+    author_email="jyk.kongkong@gmail.com",
+    description="Zhongkui file package",
+    keywords="Zhongkui file",
+    license="MIT",
+    packages=find_packages('src/python'),
+    package_dir={'': 'src/python'},
+    include_package_data=True,
+    namespace_packages=['zhongkui'],
+    install_requires=[
+        "file-magic",
+        "pefile",
+        "pytest"
+    ])
