6ee06163931d172cadf933c9057e7ffad1e1d55e
jyker@dell <jyk.kongkong@gmail.com>
2019-09-11 13:22:39 +0800

    fix install_requires

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     namespace_packages=['zhongkui'],
     cmdclass={"test": PyTest},
     install_requires=[
-        "file-magic >= 0.4.0", "pefile >= 2019.4.18", "pytest >= 5.0.1",
+        "file-magic >= 0.4.0", "pefile >= 2019.4.18", "pytest", "stringsifter",
         "zhongkui-logging"
     ])
